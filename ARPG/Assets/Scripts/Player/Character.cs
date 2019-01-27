a8ee3322020f64ab8a0bf998c0f639a9c7477278
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2019-01-26 22:03:37 -0500

    #6 The player can now move correctly, left right up and down are fully functional. Character controller will be updated a lot throughout the course of this project

---
+++ ARPG/Assets/Scripts/Player/Character.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Abstract meaning it cant exist on its own
+public abstract class Character : MonoBehaviour
+{
+
+	[SerializeField]
+	private float speed;
+	protected Vector2 direction;
+	private Rigidbody2D characterRigid;
+	
+	void Start()
+	{
+		//Initializes the rigidbody
+		characterRigid = GetComponent<Rigidbody2D>();
+	}
+
+	// Virtual update so it can be overridden
+	protected virtual void Update()
+	{
+		MoveChar();
+	}
+
+	//Moves a character
+	public void MoveChar()
+	{
+		characterRigid.velocity = direction.normalized * speed;
+
+	}
+
+}
