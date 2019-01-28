0abf6c32030756a2744a40cb156dd7c20c0ed21d
JustinMaure3 <justin.maure01@stclairconnect.ca>
2019-01-28 12:38:08 -0500

    #3 Created the title screen and added 3 basic buttons (start game, options and exit game). As well as a script to utilize these buttons

---
+++ ARPG/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+
+    //Start Game Function
+    public void StartGame () {
+        //Loads the main scene
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    //Exit Game Function
+    public void ExitGame () {
+        //Does no work while in unity only once deployed
+        Application.Quit();
+    }
+}
