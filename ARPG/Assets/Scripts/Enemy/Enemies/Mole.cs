4ff7126f45ac6d8fcdd651f5cd491dfd3a336ba1
Justin <justin.maure01@stclairconnect.ca>
2019-03-12 15:03:47 -0400

    #26 Fixed and added enemy animations

--- ARPG/Assets/Scripts/Enemy/Enemies/Mole.cs
+++ ARPG/Assets/Scripts/Enemy/Enemies/Mole.cs
@@ -29,9 +29,36 @@ public class Mole : Enemy {
 
                 if (currentState == EnemyState.idle || currentState == EnemyState.walking && currentState != EnemyState.stagger) {
                     Vector3 temp = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+                    ChangeAnimation(temp - transform.position);
                     rb.MovePosition(temp);
                     ChangeState(EnemyState.walking);
+                    animator.SetBool("IsWalking", true);
                 }
+            } else {
+                animator.SetBool("IsWalking", false);
+            }
+        }
+    }
+
+    private void SetAnimatorFloat(Vector2 setVector) {
+        animator.SetFloat("x", setVector.x);
+        animator.SetFloat("y", setVector.y);
+    }
+
+    private void ChangeAnimation(Vector2 direction) {
+        //animator.SetFloat("x", direction.x);
+        //animator.SetFloat("y", direction.y);
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) {
+            if (direction.x > 0) {
+                SetAnimatorFloat(Vector2.right);
+            } else if (direction.x < 0) {
+                SetAnimatorFloat(Vector2.left);
+            }
+        } else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y)) {
+            if (direction.y > 0) {
+                SetAnimatorFloat(Vector2.up);
+            } else if (direction.y < 0) {
+                SetAnimatorFloat(Vector2.down);
             }
         }
     }
