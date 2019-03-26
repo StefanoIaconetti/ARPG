2c9c9f5a301ec99fcb8daafbf2282f56de431477
Justin <justin.maure01@stclairconnect.ca>
2019-03-26 13:52:44 -0400

    #64 Created a quest giver NPC that will give a single quest to the player when the player interacts with it

--- ARPG/Assets/Scripts/NPC/QuestGiver.cs
+++ ARPG/Assets/Scripts/NPC/QuestGiver.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestGiver : MonoBehaviour {
+public class QuestGiver : NPC {
 
     public Quest quest;
 
     public Player player;
+    public bool questGiven = false;
 
     public void AcceptQuest() {
-        quest.isActive = true;
-        player.questList.Add(quest);
+        if(!questGiven) {
+            quest.isActive = true;
+            player.questList.Add(quest);
+            questGiven = true;
+        }
+    }
+
+    public QuestGiver() {
+        nameOfCharacter = "questgiver";
+        npcType = NPCType.QuestGiver;
+    }
+
+    public override void Triggered() {
+        base.Triggered();
+        AcceptQuest();
     }
 }
