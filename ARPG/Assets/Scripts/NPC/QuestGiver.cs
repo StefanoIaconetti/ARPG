b9403bc9d78ad77cb6eebe3776eb5b7931076f58
Justin <justin.maure01@stclairconnect.ca>
2019-03-28 12:58:26 -0400

    #64 Added different level questgivers and quests. Quests are now generated depending on level

--- ARPG/Assets/Scripts/NPC/QuestGiver.cs
+++ ARPG/Assets/Scripts/NPC/QuestGiver.cs
@@ -8,6 +8,7 @@ public class QuestGiver : NPC {
 
     public Player player;
     public bool questGiven = false;
+    public int level;
 
     private void Start() {
         CreateQuest();
@@ -32,12 +33,38 @@ public class QuestGiver : NPC {
     }
 
     public void CreateQuest() {
+        if (level > 0 && level <= 3) {
+            GenerateKillQuest(level);
+        }
+    }
+
+    public void GenerateKillQuest(int level) {
+        int requiredNum = 0;
+        int goldReward = 0;
+        int xpReward = 0;
+
         quest = new Quest();
         quest.goal.goalType = GoalType.Kill;
-        quest.goal.requiredAmount = 3;
-        quest.goldReward = 100;
-        quest.xpReward = 100;
-        quest.description = "A simple bounty";
-        quest.title = "Test Quest";
+
+        if (level == 1) {
+            requiredNum = Random.Range(1,3);
+            goldReward = Random.Range(100, 200);
+            xpReward = Random.Range(50, 100);
+        } else if (level == 2) {
+            requiredNum = Random.Range(5, 10);
+            goldReward = Random.Range(200, 400);
+            xpReward = Random.Range(100, 200);
+        } else if (level == 3) {
+            requiredNum = Random.Range(20, 50);
+            goldReward = Random.Range(400, 600);
+            xpReward = Random.Range(400, 500);
+        }
+
+        quest.goal.requiredAmount = requiredNum;
+        quest.goldReward = goldReward;
+        quest.xpReward = xpReward;
+        quest.description = "Defeat " + requiredNum + " enemies!";
+        quest.title = "Bounty Lvl: " + level;
     }
+    
 }
