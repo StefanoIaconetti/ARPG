9b8a59f41af94d509cf74f00c8645c106ac63fb5
Justin <justin.maure01@stclairconnect.ca>
2019-04-07 11:49:51 -0400

    Created a generic NPC and updated its animations to match our new npc assets

---
+++ ARPG/Assets/Scripts/NPC/GenericNPC.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericNPC : NPC {
+
+
+    public GenericNPC() {
+        nameOfCharacter = "genericnpc";
+        npcType = NPCType.NPC;
+    }
+
+
+}
