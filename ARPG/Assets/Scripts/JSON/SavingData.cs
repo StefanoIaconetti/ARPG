bf3ec323c450f38e52220dcc29522c1fdd72226e
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2019-04-07 18:35:36 -0400

    #4 #5 currently finished the basic implementation of saving and loading. Now i will start the testing and debugging

---
+++ ARPG/Assets/Scripts/JSON/SavingData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class SavingData
+{
+	public float[] playerPosition;
+	public Inventory playerInventory;
+	public Equipable[] equipableItems;
+	public float currency;
+	public float health;
+	public float experience;
+
+	public SavingData(Player player, EquipmentManager equipManag){
+
+		playerInventory = Player.inventory;
+		equipableItems = equipManag.currentEquipment;
+		currency = player.gold;
+		health = player.health;
+		experience = player.xp;
+
+		playerPosition = new float[3];
+		playerPosition [0] = player.transform.position.x;
+		playerPosition [1] = player.transform.position.y;
+		playerPosition [2] = player.transform.position.z;
+
+	}
+}
