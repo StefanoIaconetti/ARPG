b093421a8e201d9387b632cdaad2f90a22159ff4
Justin <justin.maure01@stclairconnect.ca>
2019-04-07 13:11:35 -0400

    Added npc moving functionality

--- ARPG/Assets/Scripts/NPC/GenericNPC.cs
+++ ARPG/Assets/Scripts/NPC/GenericNPC.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class GenericNPC : NPC {
 
+    public List<GameObject> targets;
+    public bool IsWalking = false;
+    private int counter = 0;
+    public int speed;
 
     public GenericNPC() {
         nameOfCharacter = "genericnpc";
         npcType = NPCType.NPC;
+    }
+
+    private void Update() {
+        if(targets != null && IsWalking == false) {
+            while(counter < targets.Count) {
+                //Move towards the first target
+                transform.position = Vector3.MoveTowards(transform.position, targets[counter].transform.position, speed * Time.deltaTime);
+
+                //Once npc reached the target. set the target to the next target
+                if(transform.position == targets[counter].transform.position) {
+                    counter++;
+                }
+
+            }
+            //When it reaches the last target. reset the counter
+            if (counter >= targets.Count) {
+                counter = 0;
+            }
+
+        }
     }
 
 
