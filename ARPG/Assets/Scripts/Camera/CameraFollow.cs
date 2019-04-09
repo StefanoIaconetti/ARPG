8375dcfd546a5275b15b60a7c9642eb170d093d3
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2019-04-09 07:52:48 -0400

    #75 Worked on main menu, gaving it a more personalized look

---
+++ ARPG/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform target;
+
+	public float smoothSpeed = 0.125f;
+
+
+
+	void LateUpdate(){
+		float offSet = smoothSpeed * Time.deltaTime;
+		Vector3 actualTarget = new Vector3 (transform.position.x, transform.position.y, target.position.z); 
+		transform.position = Vector3.MoveTowards (actualTarget, target.position, offSet);
+	}
+}
---
+++ ARPG/Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+	public string sceneName = ""; 
+
+	public void NewGame(){
+		if(sceneName != ""){
+			SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+		}
+	}
+
+	public void LoadGame(){
+		if(sceneName != ""){
+			SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+
+			DontDestroyOnLoad (GameObject.Find("LoadChecker"));
+		}
+	}
+}
