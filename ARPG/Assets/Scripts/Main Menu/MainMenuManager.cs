e94b64504f61ad9214e2d79fa43426bff2b5ec78
nstajduhar <nicholas.stajduhar01@stclairconnect.ca>
2019-04-12 16:30:14 -0400

    Readded Credits

--- ARPG/Assets/Scripts/Main Menu/MainMenuManager.cs
+++ ARPG/Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -5,25 +5,59 @@ using UnityEngine.SceneManagement;
 //This script is for the main menu where the player will be able to start a new game, load or go through settings
 public class MainMenuManager : MonoBehaviour
 {
-	//Creates a scene name
-	public string sceneName = ""; 
+    //Creates a scene name
+    public string sceneName = "";
 
-	//This is the onclick for NewGame
-	public void NewGame(){
-		//If the scene exists then it loads the scene
-		if(sceneName != ""){
-			SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
-		}
-	}
+    public GameObject newButton = GameObject.Find("New Game Button");
+    public GameObject loadButton = GameObject.Find("Load Game Button");
+    public GameObject settingsButton = GameObject.Find("Settings Button");
+    public GameObject creditsButton = GameObject.Find("Credits Button");
+    public GameObject creditsText = GameObject.Find("CreditsText");
 
-	//This is the onclick for LoadGame
-	public void LoadGame(){
-		//If the scene exists then it loads the scene
-		if(sceneName != ""){
-			SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    //Escape will reset the buttons
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            newButton.SetActive(true);
+            loadButton.SetActive(true);
+            settingsButton.SetActive(true);
+            creditsButton.SetActive(true);
+            creditsText.SetActive(false);
+        }
+    }
+    //This is the onclick for NewGame
+    public void NewGame()
+    {
+        //If the scene exists then it loads the scene
+        if (sceneName != "")
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+    }
 
-			//Sends the loadchecker gameobject to the next scene which when the next scene
-			DontDestroyOnLoad (GameObject.Find("LoadChecker"));
-		}
-	}
+    //This is the onclick for LoadGame
+    public void LoadGame()
+    {
+        //If the scene exists then it loads the scene
+        if (sceneName != "")
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+
+            //Sends the loadchecker gameobject to the next scene which when the next scene
+            DontDestroyOnLoad(GameObject.Find("LoadChecker"));
+        }
+    }
+
+    public void CreditsReal()
+    {
+        //Disabling the buttons
+        newButton.SetActive(false);
+        loadButton.SetActive(false);
+        settingsButton.SetActive(false);
+        creditsButton.SetActive(false);
+
+        //Enabling the Credits
+        creditsText.SetActive(true);
+    }
 }
